01e0dc3f4a8db730b5a9e259a5f53eff46014b9c
Hernan <hernan@Air-de-Hernan.home>
2023-01-03 22:55:52 +0100

    post axios up

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Candidatos from './Pages/Candidatos/Candidatos';
 import Formulario from './Pages/Formulario/Formulario';
 import Home from './Pages/Home/Home';
 
+
 function App() {
   return (
     <Router>
+      
       <header>
         <Link to="/"><button>Home</button></Link>
         <Link to="/candidatos"><button>Candidatos</button></Link>
         <Link to="/formulario"><button>Formulario</button></Link>
       </header>
       <main>
-        <Routes>
-        
+        <Routes>        
           <Route path="/" element={<Home/>}/>
           <Route exact path="/candidatos" element={<Candidatos/>}/>
           <Route exact path="/formulario" element={<Formulario/>}/>
--- src/Pages/Formulario/Formulario.jsx
+++ src/Pages/Formulario/Formulario.jsx
@@ -1,32 +1,56 @@
-import { useForm } from "react-hook-form"
+import React, { useState } from 'react'
+import Axios from 'axios'
+import "./Formulario.scss"
 
-const Formulario = () => {
 
-    const {register, handleSubmit } = useForm();
+function Formulario() {
+  const url = "http://localhost:8000/candidatos/post"
+  const [data, setData] = useState({
+    nombre:"",
+    apellidos:"",
+    telefono:"",
+    mail:"",
+    cv:"",
+  })
 
-    const onSubmit = data =>{
-        console.log(data.name)
-    }
-    
+  function submit(e){
+    e.preventDefault();
+    Axios.post(url, {
+      nombre: data.nombre,
+      apellidos: data.apellidos,
+      telefono: data.telefono,
+      mail: data.mail,
+      cv: data.cv,
+    })
+  }
+  
+  function handle(e){
+    const newData = { ...data}
+    newData[e.target.id] = e.target.value
+    setData(newData)
+    console.log(newData)
 
+  }
   return (
-    <div>Formulario
-        <form onSubmit = {handleSubmit(onSubmit)}>
-            <div>
-            <h4>Nombre</h4>
-            <input type="text" {...register("name")} />
+    <div>
+        <h1>Añadir Candidatos</h1>
+        <form onSubmit={(e)=> submit(e)}>
+            <div className='bl1'>
+              <p>Nombre candidatos</p>
+              <input onChange={(e)=>handle(e)} id="nombre" value={data.nombre} type="text"></input>
+              <p>Apellidos Candidato</p>
+              <input onChange={(e)=>handle(e)} id="apellidos" value={data.apellidos} type="text"></input>
             </div>
-            <h4>Apellidos</h4>
-            <input type="text" {...register("apellidos")} />
-            <h4>Telefono</h4>
-            <input type="text" {...register("telefono")} />
-            <h4>Correo Electronico</h4>
-            <input type="text" {...register("mail")} />
-            <h4>Curriculum Vitae</h4>
-            <input type="text" {...register("cv")} />
-            <div>
-                <button>Enviar</button>
+            <div className='bl2'>
+              <p>Telefono Candidato</p>
+              <input onChange={(e)=>handle(e)} id="telefono" value={data.telefono} type="number"></input>
+              <p>Correo Electronico Candidato</p>
+              <input onChange={(e)=>handle(e)} id="mail" value={data.mail} type="text"></input>
             </div>
+            <p>Curriculum Vitae Candidato</p>
+            <input onChange={(e)=>handle(e)} id="cv" value={data.cv} type="file"></input>
+            <button>Enviar</button>
+            
         </form>
     </div>
   )
